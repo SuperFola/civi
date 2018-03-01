180df77c58e8c984879ed563b03eeaf97c5a96ed
SuperFola <folaefolc@outlook.fr>
2018-03-01 14:27:39 +0100

    First commit

---
+++ entete.php
@@ -0,0 +1,5 @@
+    <head>
+        <title>Bourse Coddity 2018</title>
+        <meta name="author" value="Alexandre Plateau" />
+        <meta charset="utf-8" />
+    </head>
---
+++ index.php
@@ -0,0 +1,6 @@
+<!doctype html:5>
+<html>
+    <?php require_once "entete.php"; ?>
+    <body>
+    </body>
+</html>
